225b70c93ff61bafc07f00b2a199917882d2236a
gutti kaveri <gutikaveri@gmail.com>
2024-06-24 19:22:14 +0530

    intro,variable,hoisting,tdz

---
+++ 19-june/script.js
@@ -0,0 +1,47 @@
+// console.log("working");
+
+// Comments : ctrl + /
+// Single Line and Multi Line Comments
+
+//Three ways of rendering(returning) data on document
+
+// 1. console.log('')
+// 2. Alert
+// 3. Prompt
+
+// alert("Hi, Welcome to Javascript")
+
+// let names = prompt("Enter you name");
+
+// ``-> backticks( string interpolation)
+// alert(`Hi, Mr. ${name}`);
+// alert("Hi, my friend " + names);
+
+
+// Variables in Javascript - Storing the information you need 
+// A variable is a container for a value, like a number we might use in a sum, or a string that we might use as part of a sentence.
+
+//Container or box where you can store different type of value or data
+
+// There are three ways of defining or creating a variable 
+// 1. var 
+// 2. let 
+// 3. const
+
+// 1995 to 2015 - var keyword to define a variable
+
+
+// Rules to define a variable name 
+// Naming 
+// 1. variable name should start with letter, $ and underscore(_)
+// $kaveri, kaveri, _kaveri --> correct
+// 123kaveri but kaveri123 meaning it cannot start with number
+// 2. myVariable and myvariable are both considered different because it is case sensitive (sharat and Sharat)
+// 3. you should not used inbuilt keywords like break, const, new, continue, let 
+
+
+// var nameOfVariable = 10;
+
+
+
+
